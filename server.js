84f409a80fc3173a4ba0397ad6d8529aca8f6e3e
Vincent Yang <VincentYang2014@gmail.com>
2016-07-07 23:35:44 -0700

    dynamic port handling

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ function handler (request,response) {
 }
 
 server = http.createServer(handler);
-server.listen(5000);  // use your port number here!
+server.listen(process.env.PORT || 5000);  // use your port number here!
